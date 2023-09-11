74c3258d608b2cdabade4afde552244d6ba6b9c0
seutil <seutil@inbox.ru>
2023-09-11 16:13:45 +0300

    Add avatar field and get_absolute_url

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,12 @@
+from django.urls import reverse
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    ...
+    avatar = models.ImageField('Аватар', upload_to='accounts', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('account_detail', kwargs={
+            'pk': self.pk,  
+        })
