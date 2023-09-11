1b107ce19c4eb22fbf1a9139f403418bd05b5854
seutil <seutil@inbox.ru>
2023-09-11 11:48:52 +0300

    Add partners images in home page and add static files

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -133,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Project specific
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
