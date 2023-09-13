a88ac664c6cf0c7402c67c676697749720392307
seutil <seutil@inbox.ru>
2023-09-13 05:21:53 +0300

    Add product and related models and register it in the admin site

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from . import models
+
+
+admin.site.site_title = 'eCommerce'
+admin.site.site_header = 'Административная панель'
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_display_links = ('name', )
+
+
+class ManufactureProductsInline(admin.TabularInline):
+    model = models.Product
+
+
+@admin.register(models.Manufacture)
+class ManufactureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+    list_display_links = ('name', )
+    inlines = [ManufactureProductsInline]
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'manufacture')
+    list_display_links = ('name',)
+    list_filter = ('category', 'manufacture')
+    search_fields = ('name', )
+    save_on_top = True
+
+
+@admin.register(models.ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_image')
+    list_display_links = ('title', )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" alt="{obj.title}" height="150">')
+
+    get_image.short_description = 'Изображение'
--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField('Название', max_length=100)
-    description = models.TextField('Описание')
+    description = models.TextField('Описание', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -15,16 +15,23 @@ class Category(models.Model):
 
 class Manufacture(models.Model):
     name = models.CharField('Имя', max_length=100)
-    description = models.TextField('Описание')
-    url = models.URLField('Сайт')
+    description = models.TextField('Описание', null=True, blank=True)
+    url = models.URLField('Сайт', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
 
 
 class Product(models.Model):
     name = models.CharField('Название', max_length=100)
     description = models.TextField('Описание', null=True, blank=True)
-    price = models.PositiveIntegerField('Стоимость', help_text='Указывать стомиость в рублях')
-    category = models.ForeignKey(Category, models.DO_NOTHING, related_name='products')
-    manufacture = models.ForeignKey(Manufacture, models.DO_NOTHING, related_name='products')
+    price = models.PositiveIntegerField('Стоимость', help_text='Указывать стоимость в рублях')
+    category = models.ForeignKey(Category, models.DO_NOTHING, related_name='products', verbose_name='Категория')
+    manufacture = models.ForeignKey(Manufacture, models.DO_NOTHING, related_name='products', verbose_name='Производитель')
 
     def __str__(self):
         return self.name
@@ -36,5 +43,9 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     title = models.CharField('Заголовок', max_length=100, null=True, blank=True)
-    image = models.ImageField('Изобржение')
-    product = models.ForeignKey(Product, models.CASCADE, related_name='images')
+    image = models.ImageField('Изображение', upload_to='product/')
+    product = models.ForeignKey(Product, models.CASCADE, related_name='images', verbose_name='Продукт')
+
+    class Meta:
+        verbose_name = 'Фотография продукта'
+        verbose_name_plural = 'Фотографии продуктов'
