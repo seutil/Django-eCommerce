11362af08a2e2390e585c16bab56e195bb9c49ee
seutil <seutil@inbox.ru>
2023-09-13 09:15:51 +0300

    Add products list view with filter form

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views import generic as views
+from django.db.models import Q, Max
 
-# Create your views here.
+from . import models
+
+
+class ProductsView(views.ListView):
+    model = models.Product
+    context_object_name = 'products'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['categories'] = models.Category.objects.all()
+        context['manufactors'] = models.Manufacture.objects.all()
+        context['max_price'] = models.Product.objects.aggregate(Max('price'))['price__max']
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.GET:
+            return models.Product.objects.all()
+
+        if max_price := self.request.GET['max_price']:
+            queryset = queryset.filter(price__range=(0, max_price))
+
+        if name := self.request.GET.get('name'):
+            queryset = queryset.filter(name__icontains=name)
+
+        if (categories := self.request.GET.getlist('category')) and '__all__' not in categories:
+            queryset = queryset.filter(category__in=categories)
+
+        if (manufactors := self.request.GET.getlist('manufactor')) and '__all__' not in manufactors:
+            queryset = queryset.filter(manufacture__in=manufactors)
+
+        return queryset
