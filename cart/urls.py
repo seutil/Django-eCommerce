9dfe7d42e44f6e67e119226e2e70a7c3e65beffe
seutil <seutil@inbox.ru>
2023-09-13 12:38:38 +0300

    Add cart application

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 
 urlpatterns = [
+    path('clear/', views.ClearCartView.as_view(), name='clear_cart'),
+    path('add/<int:product_pk>/', views.AddToCartView.as_view(), name='add_to_cart'),
+    path('delete/<int:product_pk>/', views.DeleteFromCartView.as_view(), name='delete_from_cart'),
+    path('', views.CartView.as_view(), name='cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.views import generic as views
+from django.contrib.auth import mixins as auth_mixins
 
-# Create your views here.
+from . import models
+from shop import models as shop_models
+
+
+class AddToCartView(auth_mixins.LoginRequiredMixin, views.View):
+
+    def post(self, request, product_pk):
+        models.Cart.objects.create(
+            user=request.user,
+            product=shop_models.Product.objects.get(pk=product_pk),
+        )
+        return redirect(reverse('cart'))
+
+
+class DeleteFromCartView(auth_mixins.LoginRequiredMixin, views.View):
+
+    def post(self, request, product_pk):
+        models.Cart.objects.filter(
+            user=request.user,
+            product=shop_models.Product.objects.get(pk=product_pk),
+        ).delete()
+        return redirect(reverse('cart'))
+
+
+class ClearCartView(auth_mixins.LoginRequiredMixin, views.View):
+
+    def post(self, request):
+        models.Cart.objects.delete()
+        return redirect(reverse('cart'))
+
+
+class CartView(auth_mixins.LoginRequiredMixin, views.ListView):
+    model = models.Cart
+    context_object_name = 'cart'
