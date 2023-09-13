4cb7852c6e95e4d02f948af4cb082fe78dbd76bf
seutil <seutil@inbox.ru>
2023-09-13 12:08:10 +0300

    Add cart model and add it to the admin panel

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'product')
+    list_display_links = ('pk', )
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import mixins as auth_mixins
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from shop import models as shop_models
+
+
+class Cart(auth_mixins.LoginRequiredMixin, models.Model):
+    user = models.ForeignKey(get_user_model(), models.CASCADE, related_name='cart', verbose_name='Пользователь')
+    product = models.ForeignKey(shop_models.Product, models.CASCADE, related_name='cart', verbose_name='Продукт')
+
+    class Meta:
+        verbose_name = 'Корзина пользователя'
+        verbose_name_plural = 'Корзины пользователей'
